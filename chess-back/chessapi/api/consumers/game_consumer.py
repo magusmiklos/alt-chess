7ec1a4d95495876caf914a8e46560043d74167cf
magusmiklos <mrmiklosmihalyi@gmail.com>
2025-03-23 15:25:43 +0100

    basic movements

--- chess-back/chessapi/api/consumers/game_consumer.py
+++ chess-back/chessapi/api/consumers/game_consumer.py
@@ -8,6 +8,7 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.game_group = self.scope['url_route']['kwargs']['game_group']
+        
         if self.game_group not in GameConsumer.players_by_group:
             GameConsumer.players_by_group[self.game_group] = {}
 
@@ -17,26 +18,56 @@ class GameConsumer(AsyncWebsocketConsumer):
 
         await self.assign_player_order()
 
-        board = await self.create_board(8)
+        await self.create_board()
         order = GameConsumer.players_by_group[self.game_group][self.channel_name]
         print(f"sending order player: {self.channel_name} , order: {order}")
         await self.send(json.dumps(
             {
                 "type": "send_ready_message",
                 "action": "game_ready",
-                "board": board,
+                "board": GameConsumer.players_by_group[self.game_group]["board"],
                 "order": order,
             })
         )
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(self.game_group, self.channel_name)
+        
         if (self.game_group in GameConsumer.players_by_group and
                 self.channel_name in GameConsumer.players_by_group[self.game_group]):
             del GameConsumer.players_by_group[self.game_group][self.channel_name]
+        
         print(f"Player disconnected from game group {self.game_group}: {self.channel_name}")
 
-    async def create_board(self, num):
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        action = text_data_json.get("action")
+
+        if action == "move":
+            move_from = text_data_json.get("move")[0]
+            move_to = text_data_json.get("move")[1]
+
+            if not (0 <= move_from[0] < 8 and 0 <= move_from[1] < 8 and 0 <= move_to[0] < 8 and 0 <= move_to[1] < 8):
+                await self.send(text_data=json.dumps({
+                    "type": "error",
+                    "message": "Invalid move coordinates."
+                }))
+                return
+
+            await self.make_move_on_board(move_from, move_to)
+
+            # Broadcast updated board state to all connected clients in the game group
+            await self.channel_layer.group_send(
+                self.game_group,
+                {
+                    "type": "send_game_state",
+                    "board": GameConsumer.players_by_group[self.game_group]["board"],
+                }
+            )
+            print("a move was made")
+
+    async def create_board(self):
+        # Initialize the board
         board = [
             [["K"], ["N"], ["N"], ["N"], ["N"], ["N"], ["N"], ["K"]],
             [["N"], ["N"], ["N"], ["N"], ["N"], ["N"], ["N"], ["N"]],
@@ -47,15 +78,40 @@ class GameConsumer(AsyncWebsocketConsumer):
             [["N"], ["N"], ["N"], ["N"], ["N"], ["N"], ["N"], ["N"]],
             [["K"], ["N"], ["N"], ["N"], ["N"], ["N"], ["N"], ["K"]],
         ]
-        return board
+        
+        if "board" not in GameConsumer.players_by_group[self.game_group]:
+            GameConsumer.players_by_group[self.game_group]["board"] = board
+        
+        GameConsumer.players_by_group[self.game_group]["turn"] = True
 
     async def assign_player_order(self):
         group_players = GameConsumer.players_by_group[self.game_group]
+    
         if not group_players:
             group_players[self.channel_name] = random.choice([True, False])
             print("assigning the first player")
         else:
             first_value = next(iter(group_players.values()))
             group_players[self.channel_name] = not first_value
-            print("assigning the second player",group_players)
+            print("assigning the second player", group_players)
+
+    async def make_move_on_board(self, move_from, move_to):
+        board = GameConsumer.players_by_group[self.game_group]["board"]
+
+        # Make the move on the board
+        board[move_from[0]][move_from[1]], board[move_to[0]][move_to[1]] = \
+            board[move_to[0]][move_to[1]], board[move_from[0]][move_from[1]]
+
+        # Toggle the turn after the move
+        GameConsumer.players_by_group[self.game_group]["turn"] = not GameConsumer.players_by_group[self.game_group]["turn"]
+
+    async def send_game_state(self, event):
+        board = GameConsumer.players_by_group[self.game_group]["board"]
+        
+        # Broadcast the new board state to all connected clients in the game group
+        await self.send(text_data=json.dumps({
+            "type": "game_state_update",
+            "action":"board_update",
+            "board": board,
+        }))
 
--- chess-front/src/components/Game.jsx
+++ chess-front/src/components/Game.jsx
@@ -18,6 +18,9 @@ function Game() {
     const [board, setBoard] = useState(null);
     const [playerOrder, setPlayerOrder] = useState(null);
     const [UIItems, setUIItems] = useState([]);
+    const [selectedPiece, setSelectedPiece] = useState(0);
+    const [moves, setMoves] = useState([]);
+    const [turn, setTurn] = useState(true);
 
     const { gameid } = useParams();
     const wsRef = useRef(null);
@@ -46,6 +49,11 @@ function Game() {
                         setUIItems([Bpawn, Bhorse, Bbishop, Brook])
                     }
                 };
+                if (data.action === "board_update") {
+                    console.log("board update action recived");
+                    generateBoard(data.board)
+
+                }
             };
 
             ws.onerror = (error) => {
@@ -106,21 +114,48 @@ function Game() {
     }
 
     const handleSelect = (i, j) => {
-        console.log(i, j)
+        setMoves((prevMoves) => [...prevMoves, [i, j]]);
+    }
+
+    const handleSelectPiece = (index) => {
+        setSelectedPiece(index);
+    }
+
+    const handleMove = () => {
+        if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+            const message = {
+                action: "move",
+                move: moves.slice(-2),
+                piece: selectedPiece,
+            }
+
+            wsRef.current.send(JSON.stringify(message));
+            console.log("sent move to ws")
+        }
     }
 
     return (
         <>
             <div className="min-h-screen bg-gray-900 flex items-center justify-center p-6 flex-col">
+                <div className="flex justify-end items-center w-full"><div className={`w-10 h-10 rounded-full ${turn ? "bg-white" : "bg-black"}`} /></div>
                 <div className="grid grid-cols-8">
                     {board}
                 </div>
-                <div className="flex felx-row">
+                <button onClick={handleMove} className="mt-4 px-6 py-3 bg-gradient-to-r from-gray-800 to-slate-800 text-white font-bold text-lg rounded-2xl">
+                    Move
+                </button>
+                <div className="flex felx-row mt-5">
                     {UIItems.map((item, index) => (
-                        <img src={item} className="w-32 h-32" />
+                        <div key={`piece-${index}`} className="cursor-pointer select-none flex justify-center items-center flex-col" onClick={() => handleSelectPiece(index)}>
+                            <img
+                                src={item}
+                                className={`pointer-events-none w-32 h-32 m-2 ${index === selectedPiece ? "outline outline-green-500 outline-5" : ""}`}
+                            />
+                            <p className="text-6xl text-green-500 font-extrabold mt-3">{index}</p>
+                        </div>
                     ))}
                 </div>
-            </div>
+            </div >
         </>
     )
 }
